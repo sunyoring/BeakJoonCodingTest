754afe4071a286a5c47e435c81ff60b7c9804e0c
sunyoring <sunhyo67@gmail.com>
2021-09-29 20:18:55 +0900

    for문 기초 복습 뇌 운동

---
+++ BaekJoonCodingTest/src/Level1/Test15.java
@@ -0,0 +1,43 @@
+package Level1;
+
+import java.util.Scanner;
+
+public class Test15 {
+
+	/*
+	 * 문제
+		두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+		
+		입력
+		첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+		
+		각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+		
+		출력
+		각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 테스트 케이스 번호는 1부터 시작한다.
+	 */
+	
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		int a ;
+		int b ;
+		int result[] = new int[t];
+		for(int i =0; i < t; i++) {
+			
+			a = sc.nextInt();
+			b = sc.nextInt();
+
+			result[i] = a+b;
+		}
+		
+		for(int i = 0; i < t; i++) {
+			System.out.println("Case #"+(i+1)+": " + result[i]);
+		}
+		
+	}
+
+}
